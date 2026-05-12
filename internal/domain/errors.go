a4f12464a2804eaf5f45c490ab7c0656a1b51b6e
Luis Aguilar <insomniocode@gmail.com>
2026-05-12 17:50:14 -0600

    refactor: remove unused error type

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -3,7 +3,6 @@ package domain
 import "errors"
 
 var (
-	ErrMissingArgs      = errors.New("missing arguments")
 	ErrInvalidDataType  = errors.New("invalid data type")
 	ErrInvalidOperation = errors.New("invalid operation")
 	ErrInvalidValueType = errors.New("invalid value type")
