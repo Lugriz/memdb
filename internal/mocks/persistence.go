90de1b7b0898051e87b13d54284d95ebca700921
Luis Aguilar <insomniocode@gmail.com>
2026-06-18 14:50:52 -0600

    feat: improve mock persistence

--- internal/mocks/persistence.go
+++ internal/mocks/persistence.go
@@ -14,9 +14,13 @@ var _ persistence.Persistence = &MockPersistence{}
 
 func (m *MockPersistence) Set(key string, value persistence.Value) {
 	m.SpySet.Called = true
+	m.SpySet.Params = []any{key, value}
 }
 
 func (m *MockPersistence) Get(key string) (persistence.Value, bool) {
+	m.SpyGet.Called = true
+	m.SpyGet.Params = []any{key}
+
 	r := m.SpyGet.Returns
 
 	return r[0].(persistence.Value), r[1].(bool)
